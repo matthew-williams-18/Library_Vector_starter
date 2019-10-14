d6964d15c3380b4c1aac56a75235eed8b8fb00fa
matthew.williams.18 <matthew.williams.18@cnu.edu>
2019-10-14 03:41:45 -0700

    Done, maybe; untested

--- library/fileIO.cpp
+++ library/fileIO.cpp
@@ -1,5 +1,16 @@
+#include <fstream>
+#include <stdexcept>
+#include <string>
+
 #include "../includes_usr/fileIO.h"
 using namespace std;
+
+std::string convertBookToString(book workingBook);
+std::string convertPatronToString(patron workingPatron);
+book parseInputToBook(std::string workingString);
+patron parseInputToPatron(std::string workingString);
+void removeUnwantedChars(std::string &data);
+
 /* clears, then loads books from the file filename
  * returns  COULD_NOT_OPEN_FILE if cannot open filename
  * 			NO_BOOKS_IN_LIBRARY if there are 0 entries in books
@@ -7,7 +18,75 @@ using namespace std;
  * */
 int loadBooks(std::vector<book> &books, const char* filename)
 {
+	//clear book vector
+	books.clear();
+
+	//load data from file "filename" into book vector
+	ifstream inputFile;
+	std::string file = std::string(filename);
+	inputFile.open(file.c_str(), ios::in);
+
+	if (!inputFile.is_open())
+		return COULD_NOT_OPEN_FILE;
+
+	std::string line;
+
+	while (!inputFile.eof()){
+		getline(inputFile, line);
+		book b;
+		b = parseInputToBook(line);
+		books.push_back(b);
+	}
+
+	inputFile.close();
+
+	//return correct return code
+
+	if (books.size() == 0)
+		return NO_BOOKS_IN_LIBRARY;
+
 	return SUCCESS;
+}
+
+/**
+ * takes in a string containing a line of data from an input file and attempts to parse it into a book
+ * returns the book
+ */
+book parseInputToBook(std::string workingString){
+	removeUnwantedChars(workingString);
+
+	string id, title, author, state, patronID;
+	int indexOfLastSubstr = 0;
+	for (int i = 0; i < sizeof(workingString); i++){
+		if (workingString[i] == ','){
+			if (id == UNINITIALIZED){
+				id = workingString.substr(indexOfLastSubstr, i-indexOfLastSubstr + 1);
+				indexOfLastSubstr = i+2;
+			}
+			else if (title == UNINITIALIZED){
+				title = workingString.substr(indexOfLastSubstr, i-indexOfLastSubstr + 1);
+				indexOfLastSubstr = i+2;
+			}
+			else if (author == UNINITIALIZED){
+				author = workingString.substr(indexOfLastSubstr, i-indexOfLastSubstr + 1);
+				indexOfLastSubstr = i+2;
+			}
+			else if (state == UNINITIALIZED){
+				state = workingString.substr(indexOfLastSubstr, i-indexOfLastSubstr + 1);
+				indexOfLastSubstr = i+2;
+			}
+		}
+	}
+	patronID = workingString.substr(indexOfLastSubstr);
+
+	book parsedBook;
+	parsedBook.book_id = int(id);
+	parsedBook.title = title;
+	parsedBook.author = author;
+	parsedBook.state = int(state);
+	parsedBook.loaned_to_patron_id = int(patronID);
+
+	return parsedBook;
 }
 
 /* serializes books to the file filename
@@ -17,7 +96,30 @@ int loadBooks(std::vector<book> &books, const char* filename)
  * */
 int saveBooks(std::vector<book> &books, const char* filename)
 {
+	if (sizeof(books) == 0)
+		return NO_BOOKS_IN_LIBRARY;
+
+	//open file
+	ofstream outputFile;
+	std::string file = std::string(filename);
+	outputFile.open(file.c_str(), ios::out);
+
+	if (!outputFile.is_open())
+		return COULD_NOT_OPEN_FILE;
+
+	for (book b : books){
+		outputFile << convertBookToString(b) + "\n";
+	}
+
+	outputFile.close();
+
 	return SUCCESS;
+}
+
+std::string convertBookToString(book workingBook){
+	//get book data, convert to std::string if not already that type,
+	return (std::string)workingBook.book_id + "," + workingBook.title + ","
+			+ workingBook.author + "," + (std::string)workingBook.state + "," + (std::string)workingBook.loaned_to_patron_id;
 }
 
 /* clears, then loads patrons from the file filename
@@ -27,7 +129,60 @@ int saveBooks(std::vector<book> &books, const char* filename)
  * */
 int loadPatrons(std::vector<patron> &patrons, const char* filename)
 {
+	//clear patron vector
+	patrons.clear();
+
+
+	//load data from "filename" into book vector
+	ifstream inputFile;
+	std::string file = std::string(filename);
+	inputFile.open(file.c_str(), ios::in);
+
+	if (!inputFile.is_open())
+		return COULD_NOT_OPEN_FILE;
+
+	std::string line;
+
+	while(!inputFile.eof()){
+		getline(inputFile, line);
+		patron p;
+		p = parseInputToPatron(line);
+		patrons.push_back(p);
+	}
+
+	inputFile.close();
+
+	//return correct code
+	if(patrons.size() == 0)
+		return NO_PATRONS_IN_LIBRARY;
 	return SUCCESS;
+}
+
+patron parseInputToPatron(std::string workingString){
+	removeUnwantedChars(workingString);
+
+	string id, name, numBooksCheckedOut;
+	int indexOfLastSubstr = 0;
+	for (int i = 0; i < sizeof(workingString); i++){
+		if (workingString[i] == ','){
+			if (id == UNINITIALIZED){
+				id = workingString.substr(indexOfLastSubstr, i-indexOfLastSubstr + 1);
+				indexOfLastSubstr = i+2;
+			}
+			else if (name == UNINITIALIZED){
+				name = workingString.substr(indexOfLastSubstr, i-indexOfLastSubstr + 1);
+				indexOfLastSubstr = i+2;
+			}
+		}
+	}
+	numBooksCheckedOut = workingString.substr(indexOfLastSubstr);
+
+	patron parsedPatron;
+	parsedPatron.patron_id = int(id);
+	parsedPatron.name = name;
+	parsedPatron.number_books_checked_out = int(numBooksCheckedOut);
+
+	return parsedPatron;
 }
 
 /* serializes patrons to the file filename
@@ -37,5 +192,42 @@ int loadPatrons(std::vector<patron> &patrons, const char* filename)
  * */
 int savePatrons(std::vector<patron> &patrons, const char* filename)
 {
-	return SUCCESS;
+	if (sizeof(patrons) == 0)
+			return NO_BOOKS_IN_LIBRARY;
+
+		//open file
+		ofstream outputFile;
+		std::string file = std::string(filename);
+		outputFile.open(file.c_str(), ios::out);
+
+		if (!outputFile.is_open())
+			return COULD_NOT_OPEN_FILE;
+
+		for (patron p : patrons){
+			outputFile << convertPatronToString(p) + "\n";
+		}
+
+		outputFile.close();
+
+		return SUCCESS;
+}
+
+std::string convertPatronToString(patron workingPatron){
+	return (std::string)workingPatron.patron_id + "," + workingPatron.name + "," + workingPatron.number_books_checked_out;
+}
+
+void removeUnwantedChars(std::string &data){
+	try{
+		for (int i = 0; i < sizeof(data); i++){
+			switch(data[i]){
+
+			case "\"":
+			case " ": data = data.substr(0,i) + data.substr(i+1);
+			break;
+
+			default: break;
+			}
+		}
+	}
+	catch(const std::out_of_range &e){ }
 }
--- library/library.cpp
+++ library/library.cpp
@@ -9,16 +9,22 @@
 #include "../includes_usr/fileIO.h"
 using namespace std;
 
+int nextPatronID();
+
 //NOTE: please ensure patron and book data are loaded from disk before calling the following
 //NOTE: also make sure you save patron and book data to disk any time you make a change to them
 //NOTE: for files where data is stored see constants.h BOOKFILE and PATRONFILE
+
+vector<book> bookCollection;
+vector<patron> patronList;
 
 /*
  * clear books and patrons containers
  * then reload them from disk 
  */
 void reloadAllData(){
-
+	loadBooks(bookCollection, (char*)BOOKFILE);
+	loadPatrons(patronList, (char*)PATRONFILE);
 }
 
 /* checkout a book to a patron
@@ -42,6 +48,42 @@ void reloadAllData(){
  *         TOO_MANY_OUT patron has the max number of books allowed checked out
  */
 int checkout(int bookid, int patronid){
+	reloadAllData();
+
+	//test by ID if patron is in the patronList
+	bool patronFound = false;
+	patron person;
+	for (patron p : patronList){
+		if (p.patron_id == patronid)
+			patronFound = true; person = p; break;
+	}
+	//if not on list, return PATRON_NOT_ENROLLED
+	if(!patronFound)
+		return PATRON_NOT_ENROLLED;
+
+	//test by ID if book is in the bookCollection
+	bool bookFound = false;
+	book novel;
+	int bIndex = 0;
+	for (book b : bookCollection){
+		if (b.book_id == bookid)
+			bookFound = true; novel = b; break;
+		bIndex++;
+	}
+	//if not in collection, return BOOK_NOT_IN_COLLECTION
+	if(!bookFound)
+		return BOOK_NOT_IN_COLLECTION;
+
+	if (person.number_books_checked_out >= MAX_BOOKS_ALLOWED_OUT)
+		return TOO_MANY_OUT;
+
+
+	bookCollection[bIndex].loaned_to_patron_id = patronid;
+	bookCollection[bIndex].state = OUT;
+
+	saveBooks(bookCollection, (char*)BOOKFILE);
+	savePatrons(patronList, (char*)PATRONFILE);
+
 	return SUCCESS;
 }
 
@@ -54,10 +96,33 @@ int checkout(int bookid, int patronid){
  
  * Finally save the contents of the books and patrons containers to disk
  * 
- * returns SUCCESS checkout worked
+ * returns SUCCESS check-in worked
  * 		   BOOK_NOT_IN_COLLECTION
  */
 int checkin(int bookid){
+	reloadAllData();
+
+	//test by ID if patron is in the bookCollection
+	bool bookFound = false;
+	book novel;
+	int bIndex = 0;
+	for (book b : bookCollection){
+		if (b.book_id == bookid)
+			bookFound = true; novel = b; break;
+		bIndex++;
+	}
+	//if not in collection, return BOOK_NOT_IN_COLLECTION
+	if(!bookFound)
+		return BOOK_NOT_IN_COLLECTION;
+
+	for (int i = 0; i < patronList.size(); i++){
+		if (patronList[i].patron_id == novel.loaned_to_patron_id)
+			patronList[i].number_books_checked_out--; break;
+	}
+
+	bookCollection[bIndex].loaned_to_patron_id = NO_ONE;
+	bookCollection[bIndex].state = IN;
+
 	return SUCCESS;
 }
 
@@ -71,7 +136,14 @@ int checkin(int bookid){
  *    the patron_id of the person added
  */
 int enroll(std::string &name){
-	return 0;
+	reloadAllData();
+
+	patron neu;
+	neu.patron_id = nextPatronID;
+	neu.name = name;
+	neu.number_books_checked_out = 0;
+
+	return neu.patron_id;
 }
 
 /*
@@ -79,17 +151,13 @@ int enroll(std::string &name){
  * (ie. if 3 books returns 3)
  * 
  */
-int numbBooks(){
-	return 0;
-}
+int numbBooks(){ return bookCollection.size(); }
 
 /*
  * the number of patrons in the patrons container
  * (ie. if 3 patrons returns 3)
  */
-int numbPatrons(){
-	return 0;
-}
+int numbPatrons(){ return patronList.size(); }
 
 /*the number of books patron has checked out
  *
@@ -97,7 +165,13 @@ int numbPatrons(){
  *        or PATRON_NOT_ENROLLED         
  */
 int howmanybooksdoesPatronHaveCheckedOut(int patronid){
-	return 0;
+	//test by ID if patron is in the patronList
+	for (patron p : patronList){
+		if (p.patron_id == patronid)
+			return p.number_books_checked_out;
+	}
+	//if not on list, return PATRON_NOT_ENROLLED
+	return PATRON_NOT_ENROLLED;
 }
 
 /* search through patrons container to see if patronid is there
@@ -107,6 +181,20 @@ int howmanybooksdoesPatronHaveCheckedOut(int patronid){
  *         PATRON_NOT_ENROLLED no patron with this patronid
  */
 int whatIsPatronName(std::string &name,int patronid){
-	return SUCCESS;
+	//test by ID if patron is in the patronList
+	for (patron p : patronList){
+		if (p.patron_id == patronid)
+			name = p.name; return SUCCESS;
+	}
+	//if not on list, return PATRON_NOT_ENROLLED
+	return PATRON_NOT_ENROLLED;
 }
 
+int nextPatronID(){
+	int highestIDNum = -1;  //initialize to a number certain to be lower than any possible setting for a patron ID
+	for (patron p : patronList){
+		highestIDNum = (p.patron_id > highestIDNum) ? p.patron_id : highestIDNum;
+	}
+
+	return highestIDNum+1;
+}
